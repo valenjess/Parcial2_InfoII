c57eb33b4fc4d828219129d01c5b43cb4e8d23aa
Juan-garcia19 <jesteban.garcia1@udea.edu.co>
2021-09-23 14:07:58 -0500

    se crea el primer borrador de el metodo de sobremuestreo con un fallo en la lectura de la imagen y unicamente cuando se quiere pasar a dimensiones que son multiplos de la imagen original(largo y ancho)

--- Cod_Parcial2/main.cpp
+++ Cod_Parcial2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <QImage>
+#include <vector>
 
 using namespace std;
 
@@ -7,19 +8,42 @@ int main()
 {
     string filename = "../Cod_Parcial2/Imagenes/cosa.jpg";
     QImage imag( filename.c_str() );
+    vector<int> Red;
 
 
     unsigned int X = 10;
     unsigned int Y = 19;
 
     unsigned int ancho = imag.width();
-   unsigned int largo = imag.height();
+    unsigned int largo = imag.height();
 
-    cout<<ancho<<endl;
-    cout<<largo <<endl;
-    cout<<"Rojo "<<imag.pixelColor(X,Y).red()<<endl;
-    cout<<"Verde "<<imag.pixelColor(X,Y).green()<<endl;
-    cout<<"Azul "<<imag.pixelColor(X,Y).blue()<<endl;
+
+
+    unsigned int dimensionX=16;
+    unsigned int dimensionY=16;
+
+    int cantPixRepx=dimensionX/ancho;
+    int cantPixRepY=dimensionY/largo;
+
+    if (dimensionX%ancho==0 && dimensionY%largo==0){
+        for (int i =0;i < imag.width();i++){
+            vector<int> RedX;
+            for (int j =0; j < imag.height();j++){
+
+                //cout<<"[ "<<imag.pixelColor(j,i).red() <<" , "<<imag.pixelColor(j,i).green()<<" , "<<imag.pixelColor(j,i).blue()<<endl;
+                for (int l=0;l<cantPixRepx;l++){
+                    RedX.push_back(imag.pixelColor(i,j).red());
+                }
+
+            }
+            int longitud=RedX.size();
+            for(int i=0;i < cantPixRepY;i++){
+                for(int j=0;j<longitud;j++){
+                    Red.push_back(RedX[j]);
+                }
+            }
+        }
+    }
 
 
 
