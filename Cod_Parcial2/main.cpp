99dd3ed1a82d6227b271141fda02c960f42ca41f
Juan-garcia19 <jesteban.garcia1@udea.edu.co>
2021-09-23 23:58:51 -0500

    en esta instancia se termina el submuestreo con el caso de los lados ser multiplos y en caso de que no lo sean

--- Cod_Parcial2/main.cpp
+++ Cod_Parcial2/main.cpp
@@ -19,18 +19,20 @@ int main()
 
 
 
-    unsigned int dimensionX=16;
+    unsigned int dimensionX=18;
     unsigned int dimensionY=16;
 
     int cantPixRepx=dimensionX/ancho;
     int cantPixRepY=dimensionY/largo;
 
-    if (dimensionX%ancho==0 && dimensionY%largo==0){
+    int residuoX = dimensionX % ancho, residuoY = dimensionY % largo;
+
+    if (residuoX == 0 && residuoY ==0){
         for (int i =0;i < imag.width();i++){
             vector<int> RedX;
             for (int j =0; j < imag.height();j++){
 
-                //cout<<"[ "<<imag.pixelColor(j,i).red() <<" , "<<imag.pixelColor(j,i).green()<<" , "<<imag.pixelColor(j,i).blue()<<endl;
+                cout<<"[ "<<imag.pixelColor(i,j).red() <<" , "<<imag.pixelColor(i,j).green()<<" , "<<imag.pixelColor(i,j).blue()<<endl;
                 for (int l=0;l<cantPixRepx;l++){
                     RedX.push_back(imag.pixelColor(i,j).red());
                 }
@@ -42,6 +44,39 @@ int main()
                     Red.push_back(RedX[j]);
                 }
             }
+        }
+    }
+
+    else {
+        int contX = 0;
+        for (int i =0;i < imag.width();i++){
+            vector<int> RedX;
+            for (int j =0; j < imag.height();j++){
+
+                for (int l=0;l<cantPixRepx;l++){
+                    RedX.push_back(imag.pixelColor(i,j).red());
+                }
+                if (contX < residuoX){
+                    RedX.push_back(imag.pixelColor(i,j).red());
+                    contX++;
+                }
+            }
+            int longitud=RedX.size(),contY=0;
+            if(contY < residuoY){
+                for( int i = 0; i < ( cantPixRepY + 1 ); i++){
+                    for( int j = 0; j < longitud; j++){
+                        Red.push_back(RedX[j]);
+                    }
+                }
+            }
+            else{
+                for(int i=0;i < cantPixRepY;i++){
+                    for(int j=0;j<longitud;j++){
+                        Red.push_back(RedX[j]);
+                    }
+                }
+            }
+            contY++;
         }
     }
 
