83d580780b1d829fd38a5581999606cdcb2e19e6
Juan-garcia19 <jesteban.garcia1@udea.edu.co>
2021-09-26 00:48:25 -0500

    Se añade el sistema de almacenamiento por un array[3][16][16] al sobremuestreo

--- Cod_Parcial2/main.cpp
+++ Cod_Parcial2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <QImage>
 #include <vector>
+#include <array>
 
 using namespace std;
 
@@ -8,18 +9,15 @@ int main()
 {
     string filename = "../Cod_Parcial2/Imagenes/cosa.jpg";
     QImage imag( filename.c_str() );
-    vector<int> Red;
+    vector<int> Red,Green,Blue;
 
-
-    unsigned int X = 10;
-    unsigned int Y = 19;
+    array<array<array<int,16>,16>,3> Datos{};
 
     unsigned int ancho = imag.width();
     unsigned int largo = imag.height();
 
 
-
-    unsigned int dimensionX=18;
+    unsigned int dimensionX=16;
     unsigned int dimensionY=16;
 
     int cantPixRepx=dimensionX/ancho;
@@ -27,38 +25,52 @@ int main()
 
     int residuoX = dimensionX % ancho, residuoY = dimensionY % largo;
 
+
     if (residuoX == 0 && residuoY ==0){
+        int cont=0;
         for (int i =0;i < imag.width();i++){
-            vector<int> RedX;
+            vector<int> RedX,GreenX,BlueX;
             for (int j =0; j < imag.height();j++){
 
-                cout<<"[ "<<imag.pixelColor(i,j).red() <<" , "<<imag.pixelColor(i,j).green()<<" , "<<imag.pixelColor(i,j).blue()<<endl;
+                cout<<"[ "<<imag.pixelColor(j,i).red() <<" , "<<imag.pixelColor(j,i).green()<<" , "<<imag.pixelColor(j,i).blue()<<endl;
                 for (int l=0;l<cantPixRepx;l++){
                     RedX.push_back(imag.pixelColor(i,j).red());
+                    GreenX.push_back(imag.pixelColor(i,j).green());
+                    BlueX.push_back(imag.pixelColor(i,j).blue());
                 }
 
             }
             int longitud=RedX.size();
             for(int i=0;i < cantPixRepY;i++){
                 for(int j=0;j<longitud;j++){
-                    Red.push_back(RedX[j]);
+                    //Red.push_back(RedX[j]);
+                    Datos[0][cont][j]=RedX[j];//rojo
+                    //Green.push_back(GreenX[j]);
+                    Datos[1][cont][j]=GreenX[j];//verde
+                    //Blue.push_back(BlueX[j]);
+                    Datos[2][cont][j]=BlueX[j];//azul
                 }
+                cont++;
             }
         }
     }
 
     else {
-
+        int cont=0;
         for (int i =0;i < imag.width();i++){
-            vector<int> RedX;
+            vector<int> RedX,GreenX,BlueX;
             int contX = 0;
             for (int j =0; j < imag.height();j++){
 
                 for (int l=0;l<cantPixRepx;l++){
                     RedX.push_back(imag.pixelColor(i,j).red());
+                    GreenX.push_back(imag.pixelColor(i,j).green());
+                    BlueX.push_back(imag.pixelColor(i,j).blue());
                 }
                 if (contX < residuoX){
                     RedX.push_back(imag.pixelColor(i,j).red());
+                    GreenX.push_back(imag.pixelColor(i,j).green());
+                    BlueX.push_back(imag.pixelColor(i,j).blue());
                     contX++;
                 }
             }
@@ -66,14 +78,18 @@ int main()
             if(contY < residuoY){
                 for( int i = 0; i < ( cantPixRepY + 1 ); i++){
                     for( int j = 0; j < longitud; j++){
-                        Red.push_back(RedX[j]);
+                        Datos[0][cont][j]=RedX[j];//rojo
+                        Datos[1][cont][j]=GreenX[j];//verde
+                        Datos[2][cont][j]=BlueX[j];//azul
                     }
                 }
             }
             else{
                 for(int i=0;i < cantPixRepY;i++){
                     for(int j=0;j<longitud;j++){
-                        Red.push_back(RedX[j]);
+                        Datos[0][cont][j]=RedX[j];//rojo
+                        Datos[1][cont][j]=GreenX[j];//verde
+                        Datos[2][cont][j]=BlueX[j];//azul
                     }
                 }
             }
