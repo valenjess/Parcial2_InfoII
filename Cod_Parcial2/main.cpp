e95cb463fc68ea4671c53ed424a3ff3b9b927ac4
Juan-garcia19 <jesteban.garcia1@udea.edu.co>
2021-09-26 17:49:25 -0500

    se corrigen unos errores en el caso de que la imagen no sea divisora de la matriz y ademas de corrige unos datos en el txt de salida

--- Cod_Parcial2/main.cpp
+++ Cod_Parcial2/main.cpp
@@ -2,28 +2,45 @@
 #include <QImage>
 #include <vector>
 #include <array>
+#include <fstream>
+
+#define dimension 8
+
+
 
 using namespace std;
 
+
+void SobreMuestreo( array<array<array<int,8>,8>,3> &Datos);
+void EscrituraArchivo(array<array<array<int,8>,8>,3> Datos);
+
 int main()
+{
+
+
+    array<array<array<int,8>,8>,3> Datos{};
+
+    SobreMuestreo(Datos);
+
+    EscrituraArchivo(Datos);
+
+
+    return 0;
+}
+
+void SobreMuestreo(array<array<array<int,8>,8>,3> &Datos)
 {
     string filename = "../Cod_Parcial2/Imagenes/cosa.jpg";
     QImage imag( filename.c_str() );
-    vector<int> Red,Green,Blue;
 
-    array<array<array<int,16>,16>,3> Datos{};
 
     unsigned int ancho = imag.width();
     unsigned int largo = imag.height();
 
+    int cantPixRepx=dimension/ancho;
+    int cantPixRepY=dimension/largo;
 
-    unsigned int dimensionX=16;
-    unsigned int dimensionY=16;
-
-    int cantPixRepx=dimensionX/ancho;
-    int cantPixRepY=dimensionY/largo;
-
-    int residuoX = dimensionX % ancho, residuoY = dimensionY % largo;
+    int residuoX = dimension % ancho, residuoY = dimension % largo;
 
 
     if (residuoX == 0 && residuoY ==0){
@@ -32,11 +49,11 @@ int main()
             vector<int> RedX,GreenX,BlueX;
             for (int j =0; j < imag.height();j++){
 
-                cout<<"[ "<<imag.pixelColor(j,i).red() <<" , "<<imag.pixelColor(j,i).green()<<" , "<<imag.pixelColor(j,i).blue()<<endl;
+                //cout<<"[ "<<imag.pixelColor(j,i).red() <<" , "<<imag.pixelColor(j,i).green()<<" , "<<imag.pixelColor(j,i).blue()<<endl;
                 for (int l=0;l<cantPixRepx;l++){
-                    RedX.push_back(imag.pixelColor(i,j).red());
-                    GreenX.push_back(imag.pixelColor(i,j).green());
-                    BlueX.push_back(imag.pixelColor(i,j).blue());
+                    RedX.push_back(imag.pixelColor(j,i).red());
+                    GreenX.push_back(imag.pixelColor(j,i).green());
+                    BlueX.push_back(imag.pixelColor(j,i).blue());
                 }
 
             }
@@ -57,20 +74,20 @@ int main()
 
     else {
         int cont=0;
-        for (int i =0;i < imag.width();i++){
+        for (unsigned int i =0;i < largo;i++){
             vector<int> RedX,GreenX,BlueX;
             int contX = 0;
-            for (int j =0; j < imag.height();j++){
+            for (unsigned int j =0; j < ancho;j++){
 
                 for (int l=0;l<cantPixRepx;l++){
-                    RedX.push_back(imag.pixelColor(i,j).red());
-                    GreenX.push_back(imag.pixelColor(i,j).green());
-                    BlueX.push_back(imag.pixelColor(i,j).blue());
+                    RedX.push_back(imag.pixelColor(j,i).red());
+                    GreenX.push_back(imag.pixelColor(j,i).green());
+                    BlueX.push_back(imag.pixelColor(j,i).blue());
                 }
                 if (contX < residuoX){
-                    RedX.push_back(imag.pixelColor(i,j).red());
-                    GreenX.push_back(imag.pixelColor(i,j).green());
-                    BlueX.push_back(imag.pixelColor(i,j).blue());
+                    RedX.push_back(imag.pixelColor(j,i).red());
+                    GreenX.push_back(imag.pixelColor(j,i).green());
+                    BlueX.push_back(imag.pixelColor(j,i).blue());
                     contX++;
                 }
             }
@@ -82,6 +99,7 @@ int main()
                         Datos[1][cont][j]=GreenX[j];//verde
                         Datos[2][cont][j]=BlueX[j];//azul
                     }
+                    cont++;
                 }
             }
             else{
@@ -91,13 +109,53 @@ int main()
                         Datos[1][cont][j]=GreenX[j];//verde
                         Datos[2][cont][j]=BlueX[j];//azul
                     }
+                 cont++;
                 }
             }
-            contY++;
+        }
+    }
+}
+
+void EscrituraArchivo(array<array<array<int,8>,8>,3> Datos){
+    string contenido;
+
+    for (int i = 0; i < 3; i++){
+        contenido+="{";
+        for (int j = 0; j < 8; j++){
+            contenido+="{";
+            for (int k = 0; k < 8; k++){
+                contenido+=to_string(Datos[i][j][k]);
+                if(k<7){
+                    contenido+=",";
+                }
+            }
+            contenido+="}";
+            if(j<7){
+                contenido+=",";
+            }
+        }
+        contenido+="}";
+        if(i<2){
+            contenido+=",";
+            contenido+="\n";
         }
     }
 
+    ofstream archi;
+
+    archi.open("../Cod_Parcial2/BD/MatrizLeds.txt");
+
+    if (!archi.is_open())
+       {
+         cout << "Error abriendo el archivo" << endl;
+         exit(1);
+       }
 
 
-    return 0;
+    archi << contenido;
+
+    archi.close();
+
+
+
 }
