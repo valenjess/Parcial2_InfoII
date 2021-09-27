88c20f4ebd123439f8895dbfebc3a62f62aebe37
unknown <jvalentina.gaviria@udea.edu.co>
2021-09-26 22:51:33 -0500

    Se guarda info en array tridimensional

--- Cod_Parcial2/main.cpp
+++ Cod_Parcial2/main.cpp
@@ -16,7 +16,7 @@ int main()
      */
 
 
-   string filename = "../Cod_Parcial2/Imagenes/rick2.jpg";
+   string filename = "../Cod_Parcial2/Imagenes/Japon.jpg";
    QImage imag( filename.c_str() );
 
    unsigned int ancho = imag.width();
@@ -36,6 +36,28 @@ int main()
    vector <unsigned int> Rmat;
    vector <unsigned int> Gmat;  //INFO(REDUCIDA) MATRIZ LEDS
    vector <unsigned int> Bmat;
+
+   int Info[3][8][8]{};
+   int contRGB = 0;
+   int FilRGB = 0; //8 elementos para cada fila de cada color
+   int ColRGB = 0;
+
+   /*
+
+   int Datos[3][8][8]=
+   {
+     //inserte el txt
+   {{226,226,213,213,255,255,187,206},{228,226,213,213,255,255,187,206},{196,196,187,187,184,184,183,185},{196,196,187,187,184,184,183,185},{255,255,155,155,163,163,152,255},{255,255,155,155,163,163,152,255},{255,255,255,255,160,160,255,255},{255,255,255,255,160,160,255,255}},
+   {{35,35,42,42,180,180,54,40},{35,35,42,42,180,180,54,40},{41,41,43,43,59,59,60,50},{41,41,43,43,59,59,60,50},{210,210,67,67,57,57,50,214},{210,210,67,67,57,57,50,214},{229,229,211,211,62,62,202,236},{229,229,211,211,62,62,202,236}},
+   {{53,53,50,50,166,166,37,42},{53,53,50,50,166,166,37,42},{59,59,53,53,53,53,53,54},{59,59,53,53,53,53,53,54},{229,229,83,83,69,69,61,221},{229,229,83,83,69,69,61,221},{248,248,229,229,85,85,220,244},{248,248,229,229,85,85,220,244}}
+
+   };
+
+   cout<<Datos[0][0][7]<<endl;
+   cout<<Datos[1][0][0]<<endl;
+   cout<<Datos[2][0][0]<<endl;
+   cout<<Datos[0][0][0]<<endl;
+   cout<<Datos[0][0][0]<<endl;*/
 
 
     //VECTORES CON INFO RGB -->1 vector para cada color
@@ -65,9 +87,24 @@ int main()
                     }
 
                 }
-                Rmat.push_back(pixelR/divisorProm);
-                Gmat.push_back(pixelG/divisorProm);
-                Bmat.push_back(pixelB/divisorProm);
+
+                pixelR = (pixelR/divisorProm);
+                pixelG = (pixelG/divisorProm);
+                pixelB = (pixelB/divisorProm);
+
+                //LLenando el arreglo tridimensional
+
+                Info[0][FilRGB][ColRGB] = pixelR;
+                Info[1][FilRGB][ColRGB] = pixelG;
+                Info[2][FilRGB][ColRGB] = pixelB;
+
+                ColRGB++;
+
+                if((index+1)%8 == 0){
+                    FilRGB++;
+                    ColRGB = 0;
+                }
+
 
                 pixelR=0;
                 pixelG=0;
@@ -84,24 +121,9 @@ int main()
 
             }
 
-                cont=cont+colum;
-            for(int i=0; i<Rmat.size(); i++){
-                cout<<i<<" "<<Rmat[i]<<endl;
-            }
-
-            cout<<"-----"<<endl;
-
-            for(int i=0; i<Rmat.size(); i++){
-                cout<<Gmat[i]<<endl;
-            }
-            cout<<"-----"<<endl;
-
-            for(int i=0; i<Rmat.size(); i++){
-                cout<<Bmat[i]<<endl;
-            }
-
 
     ///MEDIDAS IMAGEN NO SON DIVISIBLES ENTRE 16(MEDIDAS MATRIZ RGB'S 16*16)
      //Crear una variable modulo que represente los pixeles que sobran
+
     return 0;
 }
