5b2916d5cf551faa2735746ea753200fa43fe9b0
unknown <jvalentina.gaviria@udea.edu.co>
2021-09-27 00:48:23 -0500

    Se pasa a funcion el submuestreo 1, comienza submuestreo 2

--- Cod_Parcial2/main.cpp
+++ Cod_Parcial2/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <QImage>
+#include <fstream>
 
 using namespace std;
+
+
+void EscrituraArchivo(array<array<array<int,8>,8>,3> Info);
+void Submuestreo( array<array<array<int,8>,8>,3> &Info);
 
 int main()
 {   ///MEDIDAS IMAGEN  DIVISIBLES ENTRE 16 (MEDIDAS MATRIZ RGB'S)
@@ -15,115 +20,180 @@ int main()
      * 4 de  13 pixe
      */
 
+    array<array<array<int,8>,8>,3> Info{};
 
-   string filename = "../Cod_Parcial2/Imagenes/Japon.jpg";
-   QImage imag( filename.c_str() );
+    Submuestreo(Info);
+    EscrituraArchivo(Info);
 
-   unsigned int ancho = imag.width();
-   unsigned int largo = imag.height();
+   ///MEDIDAS NO DIVISIBLES
+   ///   int MAT = 8;
+    int largimag=20;
+    int anchimag=25;
 
-   unsigned int MAT = 8; //filas matriz
-   unsigned int Cmat = 8; //columnas matriz
+    int moduloF = largimag%MAT;
+    int cocienteF = largimag/MAT;
 
-   unsigned int fil = ancho/MAT;
-   int auxfil = fil;
-   unsigned int colum = largo/Cmat;
+    int moduloC = anchimag%MAT;
+    int cocienteC = anchimag/MAT;
 
-   vector <unsigned int> Reds;
-   vector <unsigned int> Green; //IMAGEN GRANDE
-   vector <unsigned int> Blue;
+    int vecesFil1 = moduloF;
+    int vecesFil2 = MAT - moduloF;
+    int pixFil1 = cocienteF +1 ;
+    int pixFil2 = cocienteF ;
 
-   vector <unsigned int> Rmat;
-   vector <unsigned int> Gmat;  //INFO(REDUCIDA) MATRIZ LEDS
-   vector <unsigned int> Bmat;
+    int vecesCol1 =  moduloC;
+    int vecesCol2 = MAT - moduloC;
+    int pixCol1 = cocienteC + 1;
+    int pixCol2 = MAT - moduloC;
 
-   int Info[3][8][8]{};
-   int contRGB = 0;
-   int FilRGB = 0; //8 elementos para cada fila de cada color
-   int ColRGB = 0;
-
-   /*
-
-   int Datos[3][8][8]=
-   {
-     //inserte el txt
-   {{226,226,213,213,255,255,187,206},{228,226,213,213,255,255,187,206},{196,196,187,187,184,184,183,185},{196,196,187,187,184,184,183,185},{255,255,155,155,163,163,152,255},{255,255,155,155,163,163,152,255},{255,255,255,255,160,160,255,255},{255,255,255,255,160,160,255,255}},
-   {{35,35,42,42,180,180,54,40},{35,35,42,42,180,180,54,40},{41,41,43,43,59,59,60,50},{41,41,43,43,59,59,60,50},{210,210,67,67,57,57,50,214},{210,210,67,67,57,57,50,214},{229,229,211,211,62,62,202,236},{229,229,211,211,62,62,202,236}},
-   {{53,53,50,50,166,166,37,42},{53,53,50,50,166,166,37,42},{59,59,53,53,53,53,53,54},{59,59,53,53,53,53,53,54},{229,229,83,83,69,69,61,221},{229,229,83,83,69,69,61,221},{248,248,229,229,85,85,220,244},{248,248,229,229,85,85,220,244}}
-
-   };
-
-   cout<<Datos[0][0][7]<<endl;
-   cout<<Datos[1][0][0]<<endl;
-   cout<<Datos[2][0][0]<<endl;
-   cout<<Datos[0][0][0]<<endl;
-   cout<<Datos[0][0][0]<<endl;*/
+    int objeto = 0;
+    int cont = 0;
 
 
-    //VECTORES CON INFO RGB -->1 vector para cada color
-            for(int index=0; index<ancho; index++){
-                for(int elem=0; elem<largo; elem++){
+    for (int indi=0; indi<MAT*MAT; indi++){
 
-                    Reds.push_back(imag.pixelColor(index,elem).red());
-                    Green.push_back(imag.pixelColor(index,elem).green());
-                    Blue.push_back(imag.pixelColor(index,elem).blue());
+        for(int elem=0; elem<MAT; elem++){
+            for(int indexF=0; indexF<vecesFil1; indexF++){
+                 for(int indexC=0; indexC<vecesCol1; indexC++){
+
+                    objeto = Rojo[indexF*pixCol1*(MAT)+(indexC+cont)];
 
                 }
             }
-
-            //VECTORES CON INFO RGB PARA MATRIZ -->PROMEDIO DE PIXELES
-            int pixelR=0, pixelG=0, pixelB=0;
-
-            int divisorProm = fil * colum;
-            int cont = 0;
-
-            for(int index=0; index<MAT*MAT; index++){
-                for(int elem=0; elem<fil; elem++){
-                    for(int i=0; i<colum; i++){
-                       pixelR+= Reds[elem*colum*(MAT)+(i+cont)];
-                       pixelG+= Green[elem*colum*(MAT)+(i+cont)];  //Acumuladores info pixeles
-                       pixelB+= Blue[elem*colum*(MAT)+(i+cont)];
-
-                    }
-
-                }
-
-                pixelR = (pixelR/divisorProm);
-                pixelG = (pixelG/divisorProm);
-                pixelB = (pixelB/divisorProm);
-
-                //LLenando el arreglo tridimensional
-
-                Info[0][FilRGB][ColRGB] = pixelR;
-                Info[1][FilRGB][ColRGB] = pixelG;
-                Info[2][FilRGB][ColRGB] = pixelB;
-
-                ColRGB++;
-
-                if((index+1)%8 == 0){
-                    FilRGB++;
-                    ColRGB = 0;
-                }
+        }
+    }
 
 
-                pixelR=0;
-                pixelG=0;
-                pixelB=0;
-
-                if((index+1)%MAT==0){
-                    cont=largo*auxfil;
-                    auxfil += fil;
-                }
-                else{
-                cont=cont+colum;
-
-                }
-
-            }
-
-
-    ///MEDIDAS IMAGEN NO SON DIVISIBLES ENTRE 16(MEDIDAS MATRIZ RGB'S 16*16)
-     //Crear una variable modulo que represente los pixeles que sobran
 
     return 0;
+}
+
+
+void EscrituraArchivo(array<array<array<int,8>,8>,3> Datos){
+    string contenido;
+
+    for (int i = 0; i < 3; i++){
+        contenido+="{";
+        for (int j = 0; j < 8; j++){
+            contenido+="{";
+            for (int k = 0; k < 8; k++){
+                contenido+=to_string(Datos[i][j][k]);
+                if(k<7){
+                    contenido+=",";
+                }
+            }
+            contenido+="}";
+            if(j<7){
+                contenido+=",";
+            }
+        }
+        contenido+="}";
+        if(i<2){
+            contenido+=",";
+            contenido+="\n";
+        }
+    }
+
+    ofstream archi;
+
+    archi.open("../Cod_Parcial2/BD/MatrizLeds.txt");
+
+    if (!archi.is_open())
+       {
+         cout << "Error abriendo el archivo" << endl;
+         exit(1);
+       }
+
+
+    archi << contenido;
+
+    archi.close();
+
+}
+
+void Submuestreo( array<array<array<int,8>,8>,3> &Info){
+
+
+    string filename = "../Cod_Parcial2/Imagenes/Japon.jpg";
+    QImage imag( filename.c_str() );
+
+    unsigned int ancho = imag.width();
+    unsigned int largo = imag.height();
+
+    unsigned int MAT = 8; //filas matriz
+    unsigned int Cmat = 8; //columnas matriz
+
+    unsigned int fil = ancho/MAT;
+    int auxfil = fil;
+    unsigned int colum = largo/Cmat;
+
+    vector <unsigned int> Reds;
+    vector <unsigned int> Green; //IMAGEN GRANDE
+    vector <unsigned int> Blue;
+
+    int FilRGB = 0; //8 elementos para cada fila de cada color
+    int ColRGB = 0;
+
+     //VECTORES CON INFO RGB -->1 vector para cada color
+             for(int index=0; index<ancho; index++){
+                 for(int elem=0; elem<largo; elem++){
+
+                     Reds.push_back(imag.pixelColor(index,elem).red());
+                     Green.push_back(imag.pixelColor(index,elem).green());
+                     Blue.push_back(imag.pixelColor(index,elem).blue());
+
+                 }
+             }
+
+             //VECTORES CON INFO RGB PARA MATRIZ -->PROMEDIO DE PIXELES
+             int pixelR=0, pixelG=0, pixelB=0;
+
+             int divisorProm = fil * colum;
+             int cont = 0;
+
+             for(int index=0; index<MAT*MAT; index++){
+                 for(int elem=0; elem<fil; elem++){
+                     for(int i=0; i<colum; i++){
+                        pixelR+= Reds[elem*colum*(MAT)+(i+cont)];
+                        pixelG+= Green[elem*colum*(MAT)+(i+cont)];  //Acumuladores info pixeles
+                        pixelB+= Blue[elem*colum*(MAT)+(i+cont)];
+
+                     }
+
+                 }
+
+                 pixelR = (pixelR/divisorProm);
+                 pixelG = (pixelG/divisorProm);
+                 pixelB = (pixelB/divisorProm);
+
+                 //LLenando el arreglo tridimensional
+
+                 Info[0][FilRGB][ColRGB] = pixelR;
+                 Info[1][FilRGB][ColRGB] = pixelG;
+                 Info[2][FilRGB][ColRGB] = pixelB;
+
+                 ColRGB++;
+
+                 if((index+1)%8 == 0){
+                     FilRGB++;
+                     ColRGB = 0;
+                 }
+
+
+                 pixelR=0;
+                 pixelG=0;
+                 pixelB=0;
+
+                 if((index+1)%MAT==0){
+                     cont=largo*auxfil;
+                     auxfil += fil;
+                 }
+                 else{
+                 cont=cont+colum;
+
+                 }
+
+             }
+
+
 }
